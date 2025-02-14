fcae6884e2798283739c0a47c079a7f92c0740e9
Suet Kee <suet.kee@softwire.com>
2025-02-14 10:49:21 +0000

    forms created

--- myface-react/src/create-post/createPost.tsx
+++ myface-react/src/create-post/createPost.tsx
@@ -1,18 +1,40 @@
 import './createPost.scss';
+import { useState } from "react";
 export function CreatePost ()  {
+
+    const [message, setMessage] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
   
+    const handleSubmit = async (event: { preventDefault: () => void; }) => {
+  
+      event.preventDefault();  
+      const response = await fetch('http://localhost:3001/posts/create', {  
+        method: 'POST',  
+        headers: { 'Content-Type': 'application/json' },  
+        body: JSON.stringify({
+            message,  
+            imageUrl,  
+        }),
+  
+      });  
+      const data = await response.json();  
+      console.log(data);
+  
+    };
+
+
     return (
     <div className='form-container'>
         <h1> Create a new post </h1>
-        <form method="post" action="/posts/create">       
+        <form onSubmit={handleSubmit}>       
             <ul className='form-items-ul'>
                 <li className='form-items-li'>
                     <label> <strong> Message : </strong> </label>
-                    <input type="text" id = "message" name="message" required/>
+                    <input type="text" id = "message" value={message} onChange={(event) => setMessage(event.target.value)} name="message" required/>
                 </li>
                 <li className='form-items-li'>
                     <label><strong> Image URL : </strong> </label>
-                    <input type="url" name="imageUrl" required/>
+                    <input type="url" name="imageUrl" value={imageUrl} onChange={(event) => setImageUrl(event.target.value)} required/>
                 </li>
                 <li>
                     <button className='button-submit' type="submit">Submit</button> 
@@ -23,3 +45,4 @@ export function CreatePost ()  {
     </div>
   );
 };
+
